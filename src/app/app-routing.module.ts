3c811fc871ce32e42d6e1a605b59933800fea108
C.Manuel Zarate <camazala@gmail.com>
2021-04-25 09:20:08 -0500

    modulos y ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: InicioComponent},//si el usuario no coloca nada renderizo el componente de Inicio
+  {path:"usuario",loadChildren: () => import('./components/usuario/usuario.module').then(m => m.UsuarioModule)},//si el usuario en la ruta coloca usuario cargamos ese modulo
+  {path:"**",redirectTo:"/",pathMatch:"full"}//si no coincide con ninguna ruta lo dirige al inicio y que compare toda la ruta
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
